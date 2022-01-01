d80bd5973240738923982d14b68bf1703e18be8f
JvstvsHD <fanoe@mailbox.org>
2022-01-01 14:43:39 +0100

    Add information

--- src/main/kotlin/de/jvstvshd/foxesbot/module/core/commands/InfoCommand.kt
+++ src/main/kotlin/de/jvstvshd/foxesbot/module/core/commands/InfoCommand.kt
@@ -8,17 +8,14 @@ import dev.kord.rest.builder.message.create.embed
 
 suspend fun CoreModule.infoCommand() = publicSlashCommand {
     name = "info"
-    description = "Displays information of the bot"
+    description = "Informationen über den Bot"
     action {
         respond {
             embed {
                 title = "FoxesBot"
                 url = "https://discord.gg/K5rhddJtyW"
                 author = KordUtil.createAuthor("JvstvsHD", "https://github.com/JvstvsHD", "https://avatars.githubusercontent.com/u/79066214?v=4")
-                field {
-                    name = "info"
-                    value = translate("commands.info.information", bundleName = "strings_de_DE.properties")
-                }
+                description = "Dieser Bot dient als Bot für den Chilling Foxes Discord und wird hauptsächlich als Event Bot genutzt."
             }
         }
     }
