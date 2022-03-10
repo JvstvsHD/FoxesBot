f8b41f523f46b69c7708b49d22d329882be9f43c
JvstvsHD <fanoe@mailbox.org>
2022-03-10 13:51:29 +0100

    Update org.slf4j:slf4j-api to 1.7.35

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     //logging
     implementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
-    implementation("org.slf4j:slf4j-api:1.7.35")
+    implementation("org.slf4j:slf4j-api:1.7.36")
 
     //database
     implementation("org.mariadb.jdbc:mariadb-java-client:2.7.4")
